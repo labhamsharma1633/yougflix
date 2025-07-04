c9562ccb6d4d92b807085fa9c1fbae07cc929e29
LABHAM SHARMA <122859603+labhamsharma1633@users.noreply.github.com>
2025-07-04 10:28:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Capital "D" in ReactDOM
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
